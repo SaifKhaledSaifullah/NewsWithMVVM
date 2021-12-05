44dd75bb031be23690c63461a057fb5ba16a417c
Khaled Saifullah <khaled.saif06@gmail.com>
2021-12-05 19:59:11 +0600

    room dao and entity setup

---
+++ app/src/main/java/com/saif/newswithmvvm/db/ArticleDAO.kt
@@ -0,0 +1,17 @@
+package com.saif.newswithmvvm.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.saif.newswithmvvm.Article
+
+@Dao
+interface ArticleDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertOrUpdate(article: Article):Long
+
+    @Query("SELECT * FROM articles")
+    fun getAllArticles(): LiveData<List<Article>>
+    @Delete
+    suspend fun deleteArticle(article: Article)
+
+}
